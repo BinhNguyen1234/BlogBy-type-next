44583301be4c9a9709db8f0edc2ea51d29b10b16
BinhNguyen1234 <88345612+BinhNguyen1234@users.noreply.github.com>
2022-10-12 02:41:19 +0700

    fixed path

--- Server/RouteAPI/api/post/image/index.js
+++ Server/RouteAPI/api/post/image/index.js
@@ -3,7 +3,7 @@ const isAuth = require("../../../../Middeware/Auth")
 const image = require("express").Router()
 const storageEngine = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'server/image/')
+    cb(null, 'Server/image/')
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
