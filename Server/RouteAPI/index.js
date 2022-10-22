1d75381a491a29427f73b26acebbab4104f8ec33
BinhNguyen1234 <88345612+BinhNguyen1234@users.noreply.github.com>
2022-10-22 14:47:47 +0700

    push image

--- Server/RouteAPI/index.js
+++ Server/RouteAPI/index.js
@@ -4,7 +4,6 @@ const getuser = require("./getuser")
 const user = require("./user")
 const api = require("./api")
 const fakeapi = require("./fakeapi")
-// const blog = require("./blog")
 function appendAPIRoute (app) {
     app.use("/login",Login)
     app.use("/getuser",getuser)
