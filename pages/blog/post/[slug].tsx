258fb23cbd63d6880539a9275a13351f7bb36ea1
BinhNguyen1234 <88345612+BinhNguyen1234@users.noreply.github.com>
2022-10-24 17:13:31 +0700

    add meta tag

--- pages/blog/post/[slug].tsx
+++ pages/blog/post/[slug].tsx
@@ -14,9 +14,11 @@ export async function getServerSideProps(context:any){
     console.time("querry Post with title")
     const data = await blog.findOne({
         "url":url.toLowerCase()
-    },{"title":1, "content": 1})
+    },{"title":1, "content": 1, "url":1, "imgThumbnail":1, "_id": 0})
     .then((blog:any)=>{
         return {
+            imgThumbnail: blog.imgThumbnail,
+            url: blog.url,
             title: blog.title,
             content: blog.content
         }
@@ -27,17 +29,26 @@ export async function getServerSideProps(context:any){
     })
     console.timeEnd("querry Post with title")
     return {props:{
+        imgThumbnail: data.imgThumbnail,
+        url: data.url,
         title: data.title,
         content: data.content
     }}
     
 }
 type Props = Awaited<ReturnType<typeof getServerSideProps>>
-export default function Post({title, content}: any):ReactElement {
+export default function Post({title, content,imgThumbnail, url}: any):ReactElement {
     const router = useRouter()
     
     return (<>
         <Head>
+            <meta property="og:locale" content="vi_VN"></meta>
+            <meta property="og:type" content="website"></meta>
+            <meta property="og:url" content={`http://103.161.172.66/blog/post/${url}`}></meta>
+            <meta name="description" content={`${title}`}></meta>
+            <meta property="og:description" content={`${title}`}></meta>
+            <meta property="og:image" content={`http://103.161.172.66${imgThumbnail}`}></meta>
+            <meta property="og:title" content={`${title}`}></meta>
             <title>{title}</title>
         </Head>
         <LargeContentLayout>
