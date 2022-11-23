44e4f9881999c2b6b1b8330b074a48ccee034f29
BinhNguyen1234 <88345612+BinhNguyen1234@users.noreply.github.com>
2022-11-24 04:40:14 +0700

    write log

--- Server/Config/server.js
+++ Server/Config/server.js
@@ -28,8 +28,8 @@ class App {
             });
 
             Database.connect('Blog');
-            app.use(logReq);
             app.use('/external', express.static('./Server/image'));
+            app.use(logReq);
             app.use(bodyParser.json({ limit: '50mb' }));
             app.use(bodyParser.urlencoded({ extended: true }));
             app.use(
--- Server/Middeware/LogReq.js
+++ Server/Middeware/LogReq.js
@@ -1,12 +1,28 @@
+const fs = require('fs')
+
 function logReq(req, res, next) {
    const log = {
+      time: new Date().toLocaleString('en-US'),
       method: req.method,
       hostname: req.hostname,
       path: req.path,
       ip: req.ip,
-      time: new Date().toLocaleString('en-US'),
    };
-   console.log(log);
+   if(process.env.NODE_ENV=="production"){
+    const logger = fs.createWriteStream("./Server/log/log.txt",{
+      flags: 'a' 
+    })
+   logger.write(`-----------$Start----------\n`)
+   logger.write(`|time:     ${log.time}|\n`)
+   logger.write(`|method:   ${log.method}|\n`)
+   logger.write(`|hostname: ${log.hostname}\n`)
+   logger.write(`|path:     ${log.path}\n`)
+   logger.write(`|ip:       ${log.ip}\n`)
+   logger.write(`-----------$End----------\n`)
+   logger.write(`\n`)
+   logger.write(`\n`)
+   logger.end()
    next();
+   }
 }
 module.exports = logReq;
