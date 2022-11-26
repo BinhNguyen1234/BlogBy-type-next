ea3990635043f2ece64f6810e61d377d20fe95b8
BinhNguyen1234 <88345612+BinhNguyen1234@users.noreply.github.com>
2022-11-26 16:58:09 +0700

    modify color css

--- components/UserMenu/LogOutBtn.tsx
+++ components/UserMenu/LogOutBtn.tsx
@@ -1,24 +1,10 @@
 import { ReactElement } from 'react';
-import axios from 'axios';
-import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { LOGOUT } from '../../feature/login';
 import router from 'next/router';
 import { deleteCookie } from '../../ulitlity/ManupulateCookie';
 export default function LogOutBtn(): ReactElement {
    const dispatch = useDispatch();
-   // const logOutHandle = useCallback(() => {
-   //    axios
-   //       .post('api/v1/user/logout', {
-   //          method: 'POST',
-   //       })
-   //       .then((response) => {
-   //          if (response.status === 202) {
-   //             dispatch(LOGOUT(null));
-   //             router.push('/');
-   //          }
-   //       });
-   // }, []);
    return (
       <>
          <div
