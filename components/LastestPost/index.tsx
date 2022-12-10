13b0a7b5cd84d1c52c6d285e9120309f42220e54
BinhNguyen1234 <88345612+BinhNguyen1234@users.noreply.github.com>
2022-12-11 01:33:37 +0700

    done slider

--- components/LastestPost/index.tsx
+++ components/LastestPost/index.tsx
@@ -1,12 +1,12 @@
 import { ReactElement } from 'react';
 import Style from '../../styles/components/LastestPost/LastestPost.module.sass';
-import SliderPost from './SliderPost';
+import Link from 'next/link';
 export default function LastestPost(): ReactElement {
    return (
       <>
          <div id={Style.Header}>
             <div style={{ fontSize: '2.5em' }}>Lastest post</div>
-            <div style={{ fontSize: '1.5em' }}>See all posts&rarr;</div>
+            <Link href={"blog?page=1"}><a style={{ fontSize: '1.5em',textDecoration:"none" }}>See all posts&rarr;</a></Link>
          </div>
       </>
    );
