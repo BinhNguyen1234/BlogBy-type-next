b6cd24cda44606d24ace91933aa01181d87cd346
BinhNguyen1234 <88345612+BinhNguyen1234@users.noreply.github.com>
2022-12-19 22:11:30 +0700

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -23,23 +23,6 @@ function MyApp({ Component, pageProps }) {
 
    return (
       <>
-         <Head>
-            <meta property="og:locale" content="vi_VN"></meta>
-            <meta property="og:type" content="website"></meta>
-            <meta property="og:image:alt" content={`Tee Blog`}></meta>
-            <meta property="og:url" content={`http://103.161.172.66`}></meta>
-            <meta name="description" content={`LOVE - LIGHT - FREEDOM`}></meta>
-            <meta
-               property="og:description"
-               content={`LOVE - LIGHT - FREEDOM`}
-            ></meta>
-            <meta
-               property="og:image"
-               content={`http://103.161.172.66/external/upload-name-1671459695907-641779674.png`}
-            ></meta>
-            <meta property="og:title" content={`Blog của tee`}></meta>
-            <title>Tee Blog</title>
-         </Head>
          <Layout>
             <Component {...pageProps} />
          </Layout>
