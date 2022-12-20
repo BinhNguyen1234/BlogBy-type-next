7a0ac311188fd18cf7a954e2832e6669347e4cf4
BinhNguyen1234 <88345612+BinhNguyen1234@users.noreply.github.com>
2022-12-20 14:05:52 +0700

    ok

--- components/AboutMeSection/LeftContent.tsx
+++ components/AboutMeSection/LeftContent.tsx
@@ -6,10 +6,9 @@ export default function LeftContent(): ReactElement {
          <div id={Style.LeftContent}>
             <div>Hello, Tee here...</div>
             <div>
-               Tee là một phân mảnh đang trong quá trình thức tỉnh tâm linh và
-               đang tận hưởng tất cả những điều thú vị nhất trong giai đoạn
-               chuyển hóa quan trọng này, đây cũng chính là lí do để bạn Blog ra
-               đời sau rất nhiều năm ấp ủ
+            Tee là một phân mảnh đang trong quá trình thức tỉnh tâm linh
+             và đang tận hưởng tất cả những điều thú vị nhất trong giai đoạn chuyển hóa
+              quan trọng này. Đây cũng chính là lí do để bạn Blog ra đời sau vài năm ấp ủ…
             </div>
             <button type="button" className="btn btn-primary">
                Read more
